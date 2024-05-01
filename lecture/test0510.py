24865e210ac53acba3f602836cd2a04026c4e37f
ga111o <ga111o@proton.me>
2024-05-01 21:39:14 +0900

    💩 poop

---
+++ lecture/test0510.py
@@ -0,0 +1,25 @@
+age = 16
+
+# if age < 20:
+#   print("학생")
+
+# if 1:
+#   print("1")
+
+# if 0:
+#   print("1")
+
+# if True:
+#   print("True")
+
+# if age<10:
+#   print("어린이")
+
+# elif age>=10 and age<20:
+#   print("학생")
+
+# elif age >= 20:
+#   print("성인")
+
+if 10 - 5 - 10:
+  print("what")
