384aeb2fa5bd2a3d418a6a8200a7178853877af4
ga111o <ga111o@proton.me>
2024-05-02 10:56:12 +0900

    💩 poop

--- lecture/test0510.py
+++ lecture/test0510.py
@@ -21,5 +21,26 @@ age = 16
 # elif age >= 20:
 #   print("성인")
 
-if 10 - 5 - 10:
-  print("what")
+# if 10 - 5 - 10:
+#   print("what")
+
+# num = 13
+
+# if (num % 2):
+#   print("홀수")
+# else:
+#   print("짝수")
+
+year = int(input("연도를 입력하세요: "))
+
+# 윤년 판별
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"{year}년 윤년 O")  # 400으로 나누어떨어지면 윤년이다.
+        else:
+            print(f"{year}년 윤년 X")  # 100으로 나누어떨어지지만 400으로 나누어떨어지지 않으면 윤년이 아니다.
+    else:
+        print(f"{year}년 윤년 O")  # 4로 나누어떨어지고 100으로 나누어떨어지지 않으면 윤년이다.
+else:
+    print(f"{year}년 윤년 X")  # 4로 나누어떨어지지 않으면 윤년이 아니다.
