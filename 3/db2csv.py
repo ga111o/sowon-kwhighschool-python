ea30ce3fc5383ab02a5d3738975670d781bb0ffe
ga111o <ga111o@proton.me>
2024-10-31 16:28:12 +0900

    data

--- 3/db2csv.py
+++ 3/db2csv.py
@@ -4,12 +4,12 @@ import pandas as pd
 conn = sqlite3.connect('jeju2.db')
 
 query = """
-SELECT MCT_NAVER_NAME, keywords 
+SELECT MCT_NAVER_NAME, UE_AMT_GRP, keywords 
 FROM Information 
 WHERE MCT_NAVER_NAME IS NOT NULL 
 AND MCT_TYPE IS NOT NULL 
 AND keywords IS NOT NULL
-AND (UE_CNT_GRP = "6_90% 초과" OR UE_CNT_GRP = "5_75~90%")
+AND UE_AMT_GRP IS NOT NULL
 """
 
 df = pd.read_sql_query(query, conn)
